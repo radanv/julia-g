2c0edfbbaa7e20a8f7fe3e468e1ed09f326ae6c8
ravave <ravavq@gmail.com>
2022-08-05 21:45:24 -0400

    object 2D

--- core.jl
+++ core.jl
@@ -1,6 +1,22 @@
-print("Hello world")
-a = 1 + 1
-typeof(a)
+using Plots
 
-a = "Hello"
-typeof(a)
+@userplot CirclePlot
+@recipe function f(cp::CirclePlot)
+    x, y, i = cp.args
+    n = length(x)
+    inds = circshift(1:n, 1 - i)
+    linewidth --> range(0, 10, length = n)
+    seriesalpha --> range(0, 1, length = n)
+    aspect_ratio --> 1
+    label --> false
+    x[inds], y[inds]
+end
+
+n = 400
+t = range(0, 2π, length = n)
+x = 16sin.(t).^3
+y = 13cos.(t) .- 5cos.(2t) .- 2cos.(3t) .- cos.(4t)
+
+@gif for i ∈ 1:n
+    circleplot(x, y, i, line_z = 1:n, cbar = false, c = :blues, framestyle = :none)
+end when i > 40 && mod1(i, 10) == 5
